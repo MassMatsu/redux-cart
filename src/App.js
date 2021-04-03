eee99400d78152d022ae8856283e4c98ed439785
MassMatsu <masa10matsukawa@gmail.com>
2021-04-03 21:26:36 +0900

    setup Redux for the app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React from 'react';
 // components
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 // items
-import cartItems from "./cart-items";
+import cartItems from './cart-items';
 // redux stuff
+import { createStore } from 'redux';
+import reducer from './reducer/reducer';
+import { Provider } from 'react-redux';
+
+const initialStore = {
+  cart: cartItems,
+  total: 150,
+  amount: 5,
+};
+
+const store = createStore(reducer, initialStore);
 
 function App() {
   // cart setup
 
   return (
-    <main>
-      <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
+    <Provider store={store}>
+      <main>
+        <Navbar />
+        <CartContainer />
+      </main>
+    </Provider>
   );
 }
 
